813f8137ef05ffcd4f9e4baf71f817ec10c66563
Olivia Ellis <oaksellis@gmail.com>
2018-12-16 18:29:48 -0500

    more layout changes. Ready to present.

--- public/edit.js
+++ public/edit.js
@@ -1,7 +1,15 @@
 var delete_user = document.getElementById('delete_user');
 var confirm_delete = document.getElementById('confirm_delete');
-console.log(confirm_delete);
+var edit_user_settings = document.getElementById('edit_user_settings');
 
 delete_user.addEventListener('click', function() {
+  edit_user_settings.style.filter = 'blur(5px)';
   confirm_delete.style.display = 'block';
 })
+
+window.onclick = function(event) {
+if (event.target == confirm_delete) {
+  edit_user_settings.style.filter = 'none';
+  confirm_delete.style.display = 'none';
+}
+}
--- public/home.js
+++ public/home.js
@@ -1,13 +1,16 @@
 var modal = document.getElementById('login_modal');
 var login_button = document.getElementById('login_button');
 var signup_button = document.getElementById('signup_button');
+var grid = document.getElementById('grid');
 
 login_button.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = 'block';
+  grid.style.filter = 'blur(5px)';
 }
 
 window.onclick = function(event) {
 if (event.target == modal) {
+  grid.style.filter = 'none';
   modal.style.display = "none";
 }
 }
